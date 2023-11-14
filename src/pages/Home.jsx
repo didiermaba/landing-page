4c26b8ca09849b85dd748f9b0023051d4b9c4fce
didierMaba <deus_maba@yahoo.fr>
2023-11-14 15:44:13 +0100

    2nd commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Nav from "../components/Nav"
 import { Button } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Stack from '@mui/material/Stack';
 
 
 function Home() {
@@ -9,7 +12,13 @@ function Home() {
   return (
     <>
       <Nav />
-     <Button variant="contained">Hello world</Button>
+      <div className='stack'>
+     <Stack direction="row" spacing={2}>
+     <Button variant="text" color="primary" size='small'>Text</Button>
+     <Button variant="contained" color="secondary" size='large' endIcon={<SendIcon />}>Contained</Button>
+     <Button variant="outlined" color="success"size='medium'startIcon={<DeleteIcon />}>Outlinde</Button>
+     </Stack>
+     </div>
     </>
   )
 }
